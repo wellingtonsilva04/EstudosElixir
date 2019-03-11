65d16860f1ebb9cc8b84731630bf7208ecdd312d
wellingtonsilva04 <wel.dss@gmail.com>
2019-03-11 05:53:52 -0300

    ediadto o teste 'shuffling a deck randomizes it'

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -9,7 +9,7 @@ defmodule CardsTest do
 
   test "shuffling a deck randomizes it" do
     deck = Cards.create_deck
-    assert deck != Cards.shuffle(deck)  
+    refute deck == Cards.shuffle(deck)  
 
   end
 end
